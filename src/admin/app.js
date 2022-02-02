505ee7e595d2ac62f77aaa1b2b5cecb8a28b0f5d
grant <grantmossdev@gmail.com>
2022-02-02 13:44:34 -0700

    changed some fields to private

---
+++ src/admin/app.js
@@ -0,0 +1,38 @@
+// import AuthLogo from "./extensions/my-logo.png";
+// import MenuLogo from "./extensions/logo.png";
+import favicon from "./extensions/favicon.ico";
+
+export default {
+  config: {
+    // Replace the Strapi logo in auth (login) views
+    // auth: {
+    //   logo: AuthLogo,
+    // },
+    // Replace the favicon
+    head: {
+      favicon: favicon,
+    },
+    // Replace the Strapi logo in the main navigation
+    // menu: {
+    //   logo: MenuLogo,
+    // },
+    // Override or extend the theme
+    theme: {
+      colors: {
+        alternative100: "#f6ecfc",
+        alternative200: "#e0c1f4",
+        alternative500: "#ac73e6",
+        alternative600: "#9736e8",
+        alternative700: "#8312d1",
+        danger700: "#b72b1a",
+      },
+    },
+    // Extend the translations
+    // Disable video tutorials
+    tutorials: false,
+    // Disable notifications about new Strapi releases
+    notifications: { release: false },
+  },
+
+  bootstrap() {},
+};
